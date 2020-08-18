93d3a12f7470573173f981ab5784089f0cac53d1
xurongxing <xurongxing@bytedance.com>
2020-08-18 12:24:48 +0800

    --content=[123]买卖股票的最佳时机 III

--- src/com/company/Title_123/Solution.java
+++ src/com/company/Title_123/Solution.java
@@ -27,28 +27,62 @@ package com.company.Title_123;
 
 class Solution {
     public static int maxProfit(int[] prices) {
-        int result = 0;
-        for (int i = 1; i <= prices.length; i ++){
-            int temp = getMaxNum(prices, 0, i) + getMaxNum(prices, i , prices.length);
-            if (temp > result){
-                result = temp;
-            }
+        if (prices.length < 2){
+            return 0;
         }
-        return result;
+        int[][] dp = new int[prices.length][5];
+        dp[0][0] = 0;
+        dp[0][1] = - prices[0];
+
+        int length = prices.length;
+        for (int i = 1; i < length; i ++){
+            dp[i][0] = 0;
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+        return Math.max(dp[length - 1][2], dp[length - 1][4]);
     }
 
-    private static int getMaxNum(int[] prices, int start, int end) {
-        int num2 = 0;
-        for (int i = start; i < end; i++) {
-            for (int j = i + 1; j < end; j++) {
-                int temp = prices[j] - prices[i];
-                if (temp > num2 && temp > 0) {
-                    num2 = temp;
-                }
-            }
+    public static int maxProfit2(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
         }
-        return num2;
+
+        // dp[i][j] ，表示 [0, i] 区间里，状态为 j 的最大收益
+        // j = 0：什么都不操作
+        // j = 1：第 1 次买入一支股票
+        // j = 2：第 1 次卖出一支股票
+        // j = 3：第 2 次买入一支股票
+        // j = 4：第 2 次卖出一支股票
+
+        // 初始化
+        int[][] dp = new int[len][5];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        // 3 状态都还没有发生，因此应该赋值为一个不可能的数
+        for (int i = 0; i < len; i++) {
+            dp[i][3] = Integer.MIN_VALUE;
+        }
+
+        // 状态转移只有 2 种情况：
+        // 情况 1：什么都不做
+        // 情况 2：由上一个状态转移过来
+        for (int i = 1; i < len; i++) {
+            // j = 0 的值永远是 0
+            dp[i][0] = 0;
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+        // 最大值只发生在不持股的时候，因此来源有 3 个：j = 0 ,j = 2, j = 4
+        return Math.max(0, Math.max(dp[len - 1][2], dp[len - 1][4]));
     }
+
 
     public static void main(String[] args) {
         int[] prices = new int[]{3,3,5,0,0,3,1,4};
@@ -57,7 +91,7 @@ class Solution {
         System.out.println(result);
 
         prices = new int[]{1,2,3,4,5};
-        result = Solution.maxProfit(prices);
+        result = Solution.maxProfit2(prices);
 
         System.out.println(result);
 
